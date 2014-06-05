b2e3cbe962d86fa0579694a06ff970df678dd89f
Avik Pal <avikpal.me@gmail.com>
2014-06-06 00:22:11 +0530

    Update mock_player.c
    according to KDE coding style

--- mock_player.c
+++ mock_player.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2013-2014  Avik Pal<avikpal.me@gmail.com>
+ *  Copyright 2014  Avik Pal<avikpal.me@gmail.com>
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License as
@@ -24,6 +24,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+
 char CommandString[500];
 
 int main(int argc, char *argv[])
@@ -32,51 +33,58 @@ int main(int argc, char *argv[])
 Two arguments, first one is the option specifying whether recording or playing. And the second option is the
 file name to which it has to be saved/ or which has to be played back.
 */
-  printf("num args %d\n",argc);
+    printf("num args %d\n",argc);
   
-  if(argc==3 || argc==2){
+    if(argc==3 || argc==2){
   
-    if(strcmp(argv[1], "record")==0){ //start recording
-      printf("Starting recording\n");
+        if(strcmp(argv[1], "record")==0){ //start recording
+            printf("Starting recording\n");
       /* Save the recorded audio file with a default name 
        */
-      if(argc==2){
-	system("gst-launch alsasrc ! audioconvert ! audioresample ! vorbisenc ! oggmux ! filesink location= SampleTest.ogg");}
+        if(argc==2){
+            system("gst-launch alsasrc ! audioconvert ! audioresample ! 
+	        vorbisenc ! oggmux ! filesink location= SampleTest.ogg");
+        }
       
-      if(argc==3){
+        if(argc==3){
       /* Save the recorded audio file with the name provided by user 
        */
 	
-	strcpy(CommandString,"gst-launch alsasrc ! audioconvert ! audioresample ! vorbisenc ! oggmux ! filesink location=");
-	strcat(CommandString,argv[2]);
-	printf("%s", CommandString);
-	system(CommandString);
-      }
+            strcpy(CommandString,"gst-launch alsasrc ! audioconvert ! 
+	    audioresample ! vorbisenc ! oggmux ! filesink location=");
+	    strcat(CommandString,argv[2]);
+	    printf("%s", CommandString);
+	    system(CommandString);
+	
+        }
 	
 	
-    }
+        }
     
-    if(strcmp(argv[1], "playback")==0){
+        if(strcmp(argv[1], "playback")==0){
       
-      printf("Starting playback\n");
+            printf("Starting playback\n");
       /* Play the recorded audio file with the default name 
        */
-      if(argc==2)
-	system("gst-launch filesrc location=SampleTest.ogg ! decodebin ! audioconvert ! audioresample ! alsasink");
-      
+            if(argc==2){
+	        system("gst-launch filesrc location=SampleTest.ogg ! decodebin ! audioconvert ! audioresample ! alsasink");
+            }      
       /* Play recorded audio file with the specified name
        */
       
-      if(argc==3){
-	printf("starting playback\n");
-	strcpy(CommandString,"gst-launch filesrc location=");
-	strcat(CommandString, argv[2]);
-	strcat(CommandString," ! decodebin ! audioconvert ! audioresample ! alsasink");
-	system(CommandString);
+            if(argc==3){
+            
+                printf("starting playback\n");
+                strcpy(CommandString,"gst-launch filesrc location=");
+                strcat(CommandString, argv[2]);
+                strcat(CommandString," ! decodebin ! audioconvert ! audioresample ! alsasink");
+                system(CommandString);
 	
-      }
+            }
+        }
     }
-  
-  }
-  else printf("Error in input formatting");
+    
+    else{ 
+        printf("Error in input formatting");
+    }    
 }
